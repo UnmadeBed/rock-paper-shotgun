bf4500e5091f475f71282b4b7c3fa6a49420b2aa
unmadebed <kevin.archer@live.com>
2021-12-04 00:33:45 +0000

    add playRound function to determine round winner

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 // 1.Create function called 'computerPlay that randomly returns either 'rock', 'paper', or 'shotgun'. Test with console.log().
-function computerPlay() {
-	//array of available hands to play
-	const hand = ['rock', 'paper', 'shotgun'];
-	// chosen hand using random int function to access random index of hand array.
-	return hand[getRndInteger(0, 2)];
-}
-
 // Random int generator helper function
 function getRndInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// const computerSelection = computerPlay();
+function computerPlay() {
+	//array of available hands to play
+	const hand = ['rock', 'paper', 'shotgun'];
+	// hand chosen using random int function to access random index of hand array.
+	return hand[getRndInteger(0, 2)];
+}
+
+const computerSelection = computerPlay();
 
 // 2. Write function 'playRound' that takes two params 'playerSelection' and 'computerSelection', and then returns a string that declares the winner of the round like so: "You lose! Paper beats Rock.". NOTE: You are returning this result, not console.logging them. Make the playerSelection param case-insensitive.
-// function playRound(playerSelection, computerSelection) {
-//   prompt('')
-// }
+function playRound(playerSelection, computerSelection) {
+	if (
+		(playerSelection == 'rock' && computerSelection == 'paper') ||
+		(playerSelection == 'paper' && computerSelection == 'shotgun') ||
+		(playerSelection == 'shotgun' && computerSelection == 'rock')
+	) {
+		console.log(`Sorry! ${computerSelection} beats ${playerSelection}`);
+	} else if (
+		(playerSelection == 'paper' && computerSelection == 'rock') ||
+		(playerSelection == 'shotgun' && computerSelection == 'paper') ||
+		(playerSelection == 'rock' && computerSelection == 'shotgun')
+	) {
+		console.log(`Congrats! ${playerSelection} beats ${computerSelection}`);
+	} else {
+		console.log("It's a tie!");
+	}
+}
 
 // 3. Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.At this point you should be using console.log() to display the results of each round and the winner at the end.
 
